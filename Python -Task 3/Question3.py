30f69809bed6ae79904d29e0edb6e47da5af4990
kushagra-18 <46473446+kushagra-18@users.noreply.github.com>
2020-06-25 13:45:18 +0530

    Task 3 updated

--- Python -Task 3/Question3.py
+++ Python -Task 3/Question3.py
@@ -1,18 +1,58 @@
-#Question 3
-#Using for loop
+#Function are defined here
 
-print("For loop:")
-for i in range(1,11):
-    if(i!=3 and i!=7):
-        print(i)
-       
-#Using while loop
+#Name function
+def funcName(firstName,lastName):
+    fName = (firstName +" "+ lastName)
+    return (fName)
 
-print("while loop:")
-i=1
-while(i<=10):
-    if(i==3 or i==7):
-        i=i+1
-    else:
-        print(i)
-        i=i+1
+#Percent function
+
+def perMarks(listMark,noSubj):
+    totMark=sum(listMarks)
+    marksPer = totMark/noSubj
+    return marksPer
+
+
+#Marks Fucntion
+
+def totMarks(listMark):
+    totMark=sum(listMarks)
+    return totMark
+
+# Grading Function
+
+def funcGrade(marksPer):
+     if(int(markPer) >= 95):
+        return('A')
+     elif(int(markPer) >= 85 and int(markPer) <= 95):
+        return('B')
+     elif(int(markPer) >= 75 and int(markPer) <= 85):
+        return('C')
+     elif(int(markPer) >= 65 and int(markPer) <= 75):
+        return('D')
+     else:
+        return("FAILED")
+
+#Master function
+
+def masterFunc(firstName,lastName,listMarks,noSubj,marksPer):
+    print(funcName(firstName,lastName))
+    print("Your Percentage:", perMarks(listMarks,noSubj))
+    print("Your Grade:", funcGrade(marksPer))
+
+# Input
+
+firstName = input("Please Enter your first name:")
+lastName = input("Please Enter your last name:")
+noSubj = int(input("Please enter the number of subjects"))
+
+ #Declaring an empty list for marks
+    
+listMarks = []
+for i in range (0,noSubj):
+        markSubj = float(input("Enter the marks of subject:"))
+        listMarks.append(markSubj)
+marksPer = perMarks(listMarks,noSubj)
+
+#Printing
+masterFunc(firstName,lastName,listMarks,noSubj,marksPer)
